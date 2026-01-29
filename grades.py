b80cd023e789a57f9e074845ea66f0d9ade49f60
Georgi Angelov <gtangelov76@gmail.com>
2026-01-29 15:35:18 +0200

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,18 @@
+def grades(grade):
+
+    if 2.00 <= grade <= 2.99:
+        return "Fail"
+    elif 3.00<= grade <= 3.49:
+        return "Poor"
+    elif 3.50 <= grade <= 4.49:
+        return "Good"
+    elif 4.50 <= grade <= 5.49:
+        return "Very Good"
+    elif 5.50 <= grade <= 6.00:
+        return "Excellent"
+
+current_grade = float(input())
+
+result = grades(current_grade)
+
+print(result)
